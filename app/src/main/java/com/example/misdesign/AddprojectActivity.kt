5c15c37a3243e357ba61bdad815c3d9e4c7342dc
SriNithya2004 <soupatisrinithya2004@gmail.com>
2024-01-11 20:22:58 +0530

    registration,login,home are ready, rest on progress.

---
+++ app/src/main/java/com/example/misdesign/AddprojectActivity.kt
@@ -0,0 +1,11 @@
+package com.example.misdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddprojectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addproject)
+    }
+}
---
+++ app/src/main/java/com/example/misdesign/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.misdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+//        setContentView(R.layout.activity_register)
+//        setContentView(R.layout.activity_main)
+    }
+}
